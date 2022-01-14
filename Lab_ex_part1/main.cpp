b78077ff1bf8e40704d2c706783f60237a2b1a81
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-01-14 22:46:43 +0100

    timing, working random except 5 to 9

--- Lab_ex_part1/main.cpp
+++ Lab_ex_part1/main.cpp
@@ -10,7 +10,13 @@
 #include <math.h>
 #include <numeric>
 #include <fstream>
+#include <random>
+#include <algorithm>
+#include <iterator>
+#include <ctime>
 #include "cpxmacro.h"
+#include "Timer.h"
+
 
 
 using namespace std;
@@ -19,166 +25,117 @@ using namespace std;
 int status;
 char errmsg[BUF_SIZE];
 
-// data
-// const int N = 10;
-// const int I = 10;
-// const int J = 10;
-// const unsigned char nameI[I] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}; // origins
-// const unsigned char nameJ[J] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}; // destinations
-// const double C[I*J] = { 0.0,   6.0,  4.0,   7.2, 11.2, 11.0, 11.2, 11.7, 12.5, 14.2,  
-// 						6.0,   0.0,  7.2,   4.0, 13.6, 13.0, 11.7, 11.2, 11.0, 11.2,
-// 						4.0,   7.2,  0.0,   6.0,  7.3,  7.0,  7.3,  8.1,  7.3,  9.2,
-// 						7.2,   4.0,  6.0,   0.0, 10.6,  9.9,  8.6,  7.6,  7.1,  7.3,
-// 						11.2, 13.6,  7.3,  10.6,  0.0,  2.0,  4.0,  6.0,  8.0, 11.0,
-// 						11.0, 13.0,  7.0,   9.9,  2.0,  0.0,  2.0,  4.0,  6.0,  9.0,
-// 						11.2, 11.7,  7.3,   8.6,  4.0,  2.0,  0.0,  2.0,  4.0,  7.0,
-// 						11.7, 11.2,  8.1,   7.6,  6.0,  4.0,  2.0,  0.0,  2.0,  5.0,
-// 						12.5, 11.0,  7.3,   7.1,  8.0,  6.0,  4.0,  2.0,  0.0,  3.0,
-// 						14.2, 11.2,  9.2,   7.3, 11.0,  9.0,  7.0,  5.0,  3.0,  0.0 }; 
-// 						// costs(origin, destination) LINEARIZED (just an implementation choice!)
-
-// const int N = 5;
-// const int I = 5;
-// const int J = 5;
-// const unsigned char nameI[I] = { '0', '1', '2', '3', '4'}; // origins
-// const unsigned char nameJ[J] = { '0', '1', '2', '3', '4'}; // destinations
-// const double C[I*J] = { 0.0,   6.0,  4.0,   7.2, 11.2,  
-// 						6.0,   0.0,  7.2,   4.0, 13.6,
-// 						4.0,   7.2,  0.0,   6.0,  7.3,
-// 						7.2,   4.0,  6.0,   0.0, 10.6,
-// 						11.2, 13.6,  7.3,  10.6,  0.0}; 
-// 						// costs(origin, destination) LINEARIZED (just an implementation choice
-			
 const int NAME_SIZE = 512;
 char name[NAME_SIZE];
 	
 void setupLP(CEnv env, Prob lp, const int N, const std::vector<std::vector<double>>& C)
 {
-	std::vector<int> v(N) ; // vector with N ints.
-	std::iota (std::begin(v), std::end(v), 0); // Fill with 0, 1, ..., N.
-	unsigned char nameN[N];
-	for (int a = 0; a < N; a++)
-	{
-		nameN[a] = v[a]+ '0';
-	}	
-	
-	const int I = N;
-	const int J = N;
-
+	std::vector<int> nameN(N) ; // vector with N ints.
+	std::iota (std::begin(nameN), std::end(nameN), 0); // Fill with 0, 1, ..., N.
 	// add x vars [in o.f.: sum{i,j} 0 x_ij + ...]
-	for (int i = 0; i < I; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 1; j < J; j++) // no need for x_i0 forall i
+		for (int j = 1; j < N; j++) // no need for x_i0 forall i
 		{
 			char xtype = 'I';
 			double lb = 0.0;
 			double ub = CPX_INFBOUND;
-			snprintf(name, NAME_SIZE, "x_%c_%c", nameN[i], nameN[j]);
+			snprintf(name, NAME_SIZE, "x_%d_%d", nameN[i], nameN[j]);
 			char* xname = (char*)(&name[0]);
 			CHECKED_CPX_CALL( CPXnewcols, env, lp, 1   , 0, &lb, &ub, &xtype, &xname );
-			/// status =      CPXnewcols (env, lp, ccnt, obj      , lb  , ub, xctype, colname);
 		}
 	}
 	// add y vars [in o.f.: ... + sum{ij} C_ij y_ij + ... ]
-	for (int i = 0; i < I; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < J; j++)
+		for (int j = 0; j < N; j++)
 		{
 			char xtype = 'B';
 			double lb = 0.0;
 			double ub = 1.0;
-			snprintf(name, NAME_SIZE, "y_%c_%c", nameN[i], nameN[j]);
+			snprintf(name, NAME_SIZE, "y_%d_%d", nameN[i], nameN[j]);
 			char* xname = (char*)(&name[0]);
 			CHECKED_CPX_CALL( CPXnewcols, env, lp, 1   , &C[i][j], &lb, &ub, &xtype, &xname );
-			/// status =      CPXnewcols (env, lp, ccnt, obj      , lb  , ub, xctype, colname);
 		}
 	}
 	
 	///////////////////////////////////////////////////////////
 	//
 	// now variables are stored in the following order
-	//  x01 x02 ...   x11 x12 ... #xij #    ... ...   y00   y01   ...   	y10   		y11     ... #yij      	  #   ... ...  #
+	//  x01 x02 ...   x11 x12 ... #xij #    ... ...   y00   y01   ...   y10   	y11     ... #yij      	  #   ... ...  #
 	// with indexes
-	//  0   1         J-1 J      #i*(J-1)+j-1#       (I-1)*J (I-1)*J+1    (I-1)*J+J (I-1)*J+J+1     #(I-1)*J+i*J+j#     2*I*J-I#
+	//  0   1         N-1 N      #i*(N-1)+j-1#       (N-1)*N (N-1)*N+1  N*N 	N*N+1     #(N-1)*N+i*N+j#     2*N*N-N#
+	//  0   1         N-1 N      #i*(N-1)+j-1#       (N-1)*N (N-1)*N+1  N*N 	N*N+1     #N*(N+i-1)+j#       2*N*N-N#
 	//
 	///////////////////////////////////////////////////////////
-
 	// add constraints [ forall j, sum{i} y_ij = 1 ]
-	for (int j = 0; j < J; j++)
+	for (int j = 0; j < N; j++)
 	{
-		std::vector<int> idx(I);
-		std::vector<double> coef(I, 1.0);
+		std::vector<int> idx(N);
+		std::vector<double> coef(N, 1.0);
 		char sense = 'E';
-		for (int i = 0; i < I; i++)
+		for (int i = 0; i < N; i++)
 		{
-			idx[i] =  (I-1)*J + i*J + j; // corresponds to variable y_ij
+			idx[i] =  N*(N+i-1)+j; // corresponds to variable y_ij
 		}
 		int matbeg = 0;
 		double rhs = 1;
 		CHECKED_CPX_CALL( CPXaddrows, env, lp, 0     , 1     , idx.size(), &rhs, &sense, &matbeg, &idx[0], &coef[0], NULL      , NULL      );
-    	/// status =      CPXaddrows (env, lp, colcnt, rowcnt, nzcnt     , rhs  , sense , rmatbeg, rmatind, rmatval , newcolname, newrowname);
 	}
-
 	// add constraints [ forall i, sum{j} y_ij = 1 ]
-	for (int i = 0; i < I; i++)
+	for (int i = 0; i < N; i++)
 	{
-		std::vector<int> idx(J);
-		std::vector<double> coef(J, 1.0);
+		std::vector<int> idx(N);
+		std::vector<double> coef(N, 1.0);
 		char sense = 'E';
-		for (int j = 0; j < J; j++)
+		for (int j = 0; j < N; j++)
 		{
-			idx[j] = (I-1)*J + i*J + j; // corresponds to variable y_ij
+			idx[j] = N*(N+i-1)+j; // corresponds to variable y_ij
 		}
 		int matbeg = 0;
 		double rhs = 1;
 		CHECKED_CPX_CALL( CPXaddrows, env, lp, 0     , 1     , idx.size(), &rhs, &sense, &matbeg, &idx[0], &coef[0], NULL      , NULL      );
-    	/// status =      CPXaddrows (env, lp, colcnt, rowcnt, nzcnt     , rhs  , sense , rmatbeg, rmatind, rmatval , newcolname, newrowname);
 	}
-
 	// add constraints [ forall (i,j) in A, j != 0, x_ij <= (N-1)y_ij ]
 	// add constraints [ forall (i,j) in A, j != 0, x_ij + (1-N)y_ij <= 0]
-	for (int i = 0; i < I; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 1; j < J; j++)
+		for (int j = 1; j < N; j++)
 		{
 			std::vector<int> idx(2); 
-			idx[0] = i*(J-1) + j-1; // x_ij 
-			idx[1] = (I-1)*J + i*J + j; // y_ij
+			idx[0] = i*(N-1) + j-1; // x_ij 
+			idx[1] = N*(N+i-1)+j; // y_ij
+
 			std::vector<double> coef(2);
 			coef[0] = 1;
 			coef[1] = 1 - N;
+
 			char sense = 'L';
 			int matbeg = 0;
 			double rhs = 0;
 			CHECKED_CPX_CALL( CPXaddrows, env, lp, 0     , 1     , idx.size(), &rhs, &sense, &matbeg, &idx[0], &coef[0], NULL      , NULL      );	
-			/// status =      CPXaddrows (env, lp, colcnt, rowcnt, nzcnt     , rhs  , sense , rmatbeg, rmatind, rmatval , newcolname, newrowname);
 		}
 	}
-
 	// add constraints [ forall k in N, k != 0, sum(i,k) x_ik - sum(k,j) x_kj = 1, j!=0]
-	for (int k = 1; k < I; k++)
+	for (int k = 1; k < N; k++)
 	{
-		std::vector<int> idx(2*J);
-		std::vector<double> coef(2*J, 1.0);
+		std::vector<int> idx(2*N);
+		std::vector<double> coef(2*N, 1.0);
 		char sense = 'E';
-		for (int i = 0; i < I; i++)
+		for (int i = 0; i < N; i++)
 		{
-			idx[i] = i*(J-1) + k-1; // corresponds to variable x_ik
+			idx[i] = i*(N-1) + k-1; // corresponds to variable x_ik
 		}
-		for (int j = 0; j < J; j++)
+		for (int j = 0; j < N; j++)
 		{
-			idx[I+j] = k*(J-1) + j; // corresponds to variable x_kj
-			coef[I+j] = -1; 
+			idx[N+j] = k*(N-1) + j; // corresponds to variable x_kj
+			coef[N+j] = -1; 
 		}
 		int matbeg = 0;
 		double rhs = 1;
 		CHECKED_CPX_CALL( CPXaddrows, env, lp, 0     , 1     , idx.size(), &rhs, &sense, &matbeg, &idx[0], &coef[0], NULL      , NULL      );
-    	/// status =      CPXaddrows (env, lp, colcnt, rowcnt, nzcnt     , rhs  , sense , rmatbeg, rmatind, rmatval , newcolname, newrowname);
 	}
-
-	// print (debug)
-	CHECKED_CPX_CALL( CPXwriteprob, env, lp, "ex1_v1.lp", NULL );
-	/// status =      CPXwriteprob (env, lp, "myprob"    , filetype_str);
+	CHECKED_CPX_CALL( CPXwriteprob, env, lp, "ex1.lp", NULL );
 }
 
 int read(const char* filename, std::vector<std::vector<double>> & pos, std::vector< std::vector<double> > & cost)
@@ -213,16 +170,71 @@ int read(const char* filename, std::vector<std::vector<double>> & pos, std::vect
 	return n;
 }
 
+void randomCost(const int n, std::vector<std::vector<double>> & pos, std::vector< std::vector<double> > & cost)
+{
+	std::cout << "number of random nodes n = " << n << std::endl;
+	std::vector<int> v1(n) ; // vector with N ints.
+	std::iota (std::begin(v1), std::end(v1), 0); // Fill with 0, 1, ..., N.
+	std::vector<std::vector<double>> allPos;
+	allPos.resize(n*n);
+
+    // Nested loop for all possible pairs
+	int a = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            allPos[a].resize(2);
+			allPos[a][0] = i;
+			allPos[a][1] = j;
+			a++;
+        }
+		
+    }
+	// for (int i = 0; i < n*n; i++) {
+    //     std::cout << "(" << allPos[i][0] << "," << allPos[i][1] << ")\n"; 
+    // }
+	// std::cout << "All pairs done\n";
+
+	std::srand(std::time(0));
+	std::random_shuffle(allPos.begin(),allPos.end()); // shuffle all pairs
+
+	pos.resize(n);
+	for (int i = 0; i < n; i++) {
+        std::cout << "(" << allPos[i][0] << "," << allPos[i][1] << ")\n";
+		pos[i].resize(2);
+		pos[i][0] = allPos[i][0];
+		pos[i][1] = allPos[i][1]; // save postions as the first N from the N*N pairs
+    }
+
+	// compute costs
+	cost.resize(n);
+	for (int i = 0; i < n; i++) {
+		cost[i].reserve(n);
+		for (int j = 0; j < n; j++) {
+			double c = abs(pos[i][0]-pos[j][0]) + abs(pos[i][1]-pos[j][1]); // Manhatan distance
+			// double c = sqrt(pow((pos[i][0]-pos[j][0]),2) + pow((pos[i][1]-pos[j][1]),2)); // Euclidean distance
+			// std::cout << "(" << i << "," << j << ") -> " << c << std::endl;
+			cost[i].push_back(c);
+		}
+	}
+	return;
+}
+
 
 int main (int argc, char const *argv[])
 {
 	try
 	{
 
-		if (argc < 2) throw std::runtime_error("usage: ./main filename.dat");
+		if (argc < 2) throw std::runtime_error("usage: ./main filename.dat [Nrandom]");
 		std::vector<std::vector<double>> cost;
 		std::vector<std::vector<double>> pos;
-		int N = read(argv[1],pos,cost);
+		int N = 0;
+		if (argc == 3)
+		{
+			N = atoi(argv[2]);
+			randomCost(N,pos,cost);
+		}	
+		else N = read(argv[1],pos,cost);
 
 
 		// init
@@ -231,7 +243,12 @@ int main (int argc, char const *argv[])
 		// setup LP
 		setupLP(env, lp, N, cost);
 		// optimize
-		CHECKED_CPX_CALL(CPXmipopt, env, lp );
+		using Log::Timer;
+		Timer t;
+		CHECKED_CPX_CALL(CPXmipopt, env, lp ); // SEG FAULT for n=5,6,7,8,9
+		std::cout << "Time: " << t.stop()/1000 << " s" << std::endl;
+		// std::cout << "Time: " << t.stop() << " s" << std::endl;
+		
 		// print
 		double objval;
 		CHECKED_CPX_CALL(CPXgetobjval, env, lp, &objval );
@@ -243,11 +260,11 @@ int main (int argc, char const *argv[])
   		CHECKED_CPX_CALL( CPXgetx, env, lp, &varVals[0], 0, n - 1 );
 		/// status =      CPXgetx (env, lp, x          , 0, CPXgetnumcols(env, lp)-1);
   		for ( int i = 0 ; i < n ; ++i ) {
-  	  	std::cout << "var in position " << i << " : " << varVals[i] << std::endl;
+  	  	// std::cout << "var in position " << i << " : " << varVals[i] << std::endl;
   	  		/// to read variables names the API function ``CPXgetcolname'' may be used (it is rather tricky, see the API manual if you like...)
   	  		/// status = CPXgetcolname (env, lp, cur_colname, cur_colnamestore, cur_storespace, &surplus, 0, cur_numcols-1);
   		}
-		CHECKED_CPX_CALL( CPXsolwrite, env, lp, "ex1_v1.sol" );
+		CHECKED_CPX_CALL( CPXsolwrite, env, lp, "ex1.sol" );
 		// free
 		CPXfreeprob(env, &lp);
 		CPXcloseCPLEX(&env);
