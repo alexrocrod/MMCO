dfe7bc5133f5b99b0b4c50a3b199ea199dd42933
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-01-19 12:35:15 +0100

    easier comand line options, heu1 from 20 to 26

--- Lab_ex_part2/main.cpp
+++ Lab_ex_part2/main.cpp
@@ -5,8 +5,6 @@
 
 
 #include <stdexcept>
-// #include <ctime>
-// #include <sys/time.h>
 
 #include "TSPSolver.h"
 #include "Timer.h"
@@ -20,13 +18,17 @@ int main (int argc, char const *argv[])
 {
   try
   {
-    if (argc < 4 ) throw std::runtime_error("usage: ./main filename.dat tabulength maxiter [readPos] [Nrandom] [initHeu]");  /// new parameters for TS
-    int tabuLength = atoi(argv[2]);                                                           /// new parameters for TS
-    int maxIter    = atoi(argv[3]);                                                           /// new parameters for TS
+    if (argc < 4 ) throw std::runtime_error("usage: ./main filename.dat tabulength maxiter [init] [readPos] [Nrandom] "); 
+    // filename not used if random, readPos: file to read positions, Nrandom: random number of nodes, initHeu: enable initial heuristic solution
+    int tabuLength = atoi(argv[2]);                                                           
+    int maxIter    = atoi(argv[3]);                                                           
     TSP tspInstance; 
-    if (argc == 5)
+    int init = 0; // 0 for random, 1 for initHeu1
+    if (argc > 4) // get required initalization method
+      init = atoi(argv[4]);
+    if (argc == 6) // read positions instead of costs
       tspInstance.readPos(argv[1]);
-    else if (argc >= 6)
+    else if (argc > 6) // use random cost matrix with N nodes
 		{
 			int N = atoi(argv[5]);
 			tspInstance.randomCost(N);
@@ -35,25 +37,18 @@ int main (int argc, char const *argv[])
       tspInstance.read(argv[1]);
 
     TSPSolution aSolution(tspInstance);
-    
-    // clock_t t1,t2;
-    // t1 = clock();
-    // struct timeval  tv1, tv2;
-    // gettimeofday(&tv1, NULL);
-    Log::Timer tnew;
 
-    
-    TSPSolver tspSolver;
-    if (argc == 7) tspSolver.initHeu1(tspInstance,aSolution);
+    Log::Timer tnew; // start times
+
+    TSPSolver tspSolver; // initialization
+    if (init != 0) tspSolver.initHeu1(tspInstance,aSolution);
     else tspSolver.initRnd(aSolution);
-    // 
+    
 
     TSPSolution bestSolution(tspInstance);
-    tspSolver.solve(tspInstance,aSolution, tabuLength/*new*/ , maxIter/*new*/ ,bestSolution); /// new parameters for TS
+    tspSolver.solve(tspInstance,aSolution, tabuLength, maxIter ,bestSolution); /// solve with TSAC
 
-    // t2 = clock();
-    // gettimeofday(&tv2, NULL);
-    double micros = tnew.stopMicro();
+    double micros = tnew.stopMicro(); 
 		
     std::cout << "FROM solution: "; 
     aSolution.print();
@@ -61,8 +56,6 @@ int main (int argc, char const *argv[])
     std::cout << "TO   solution: "; 
     bestSolution.print();
     std::cout << "(value : " << tspSolver.evaluate(bestSolution,tspInstance) << ")\n";
-    // std::cout << "in " << (double)(tv2.tv_sec+tv2.tv_usec*1e-6 - (tv1.tv_sec+tv1.tv_usec*1e-6)) << " seconds (user time)\n";
-    // std::cout << "in " << (double)(t2-t1) / CLOCKS_PER_SEC << " seconds (CPU time)\n";
     std::cout << "in " << micros*1e-6 << " seconds (chrono time)\n";
     
   }
