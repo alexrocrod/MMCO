7f41e86a1954ea7dfa3eefe9b17f286a67338933
Alexandre Rodrigues <alexandrerrodrigues@ua.pt>
2022-01-20 00:24:31 +0100

    all instances, missing 100 2 result

--- Lab_ex_part1/run.sh
+++ Lab_ex_part1/run.sh
@@ -8,6 +8,9 @@ echo 'N =' $1 >  Results/results$1_class$2.txt  # clean results file
 mkdir SavedDists/n$1_class$2
 for ((i=0; i<$MAX; i++))
 do 
+    # echo $i
+    printf '%d, ' $i;
     ./main rand SavedDists/n$1_class$2/$i.dat x $1 $2 >> Results/results$1_class$2.txt
 done
+printf '\n';
 #  usage: ./main filename.dat savedistsfile.dat [readDists] [Nrandom] [class]
